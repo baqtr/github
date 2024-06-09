d8d9eb49b2a653c951aa017a01a21039ba0cce36
baqtr <102158404+baqtr@users.noreply.github.com>
2024-06-09 19:59:55 +0300

    Update gg.py

--- gg.py
+++ gg.py
@@ -3,8 +3,9 @@ import os
 import psycopg2
 from telebot import types
 
+# يجب تغيير هذا إلى TOKEN الخاص بك
 TOKEN = '7031770762:AAEKh2HzaEn-mUm6YkqGm6qZA2JRJGOUQ20'
-database_url = os.getenv("DATABASE_URL", "postgres://u7sp4pi4bkcli5:p8084ef55d7306694913f43fe18ae8f1e24bf9d4c33b1bdae2e9d49737ea39976@ec2-18-210-84-56.compute-1.amazonaws.com:5432/dbdstma1phbk1e")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://u7sp4pi4bkcli5:p8084ef55d7306694913f43fe18ae8f1e24bf9d4c33b1bdae2e9d49737ea39976@ec2-18-210-84-56.compute-1.amazonaws.com:5432/dbdstma1phbk1e")
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -32,7 +33,12 @@ def save_text(message):
     text = message.text
     cursor.execute("INSERT INTO texts (user_id, text) VALUES (%s, %s)", (user_id, text))
     conn.commit()
-    bot.send_message(message.chat.id, "تمت إضافة النص بنجاح!")
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('إضافة نص')
+    btn2 = types.KeyboardButton('عرض النصوص')
+    btn_back = types.KeyboardButton('رجوع')
+    markup.add(btn1, btn2, btn_back)
+    bot.send_message(message.chat.id, "تمت إضافة النص بنجاح!", reply_markup=markup)
 
 @bot.message_handler(func=lambda message: message.text == 'عرض النصوص')
 def show_texts(message):
@@ -43,7 +49,20 @@ def show_texts(message):
         response = '\n'.join([text[0] for text in texts])
     else:
         response = "لا يوجد نصوص محفوظة."
-    bot.send_message(message.chat.id, response)
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('إضافة نص')
+    btn2 = types.KeyboardButton('عرض النصوص')
+    btn_back = types.KeyboardButton('رجوع')
+    markup.add(btn1, btn2, btn_back)
+    bot.send_message(message.chat.id, response, reply_markup=markup)
+
+@bot.message_handler(func=lambda message: message.text == 'رجوع')
+def go_back(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('إضافة نص')
+    btn2 = types.KeyboardButton('عرض النصوص')
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, "مرحباً! اختر إجراءً:", reply_markup=markup)
 
 if __name__ == '__main__':
     bot.polling()
