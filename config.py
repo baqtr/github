8a21968bd1239e9c7028bda071a46f71765ba335
baqtr <102158404+baqtr@users.noreply.github.com>
2024-10-20 11:21:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE", "")
+    TG_BOT_TOKEN = os.environ.get("7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE", "7013440973")
 
     APP_ID = int(os.environ.get("APP_ID", 21669021))
 
