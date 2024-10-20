bd5bb8d3a05a19c9a44fabc410f7d95f593ad3ad
baqtr <102158404+baqtr@users.noreply.github.com>
2024-10-20 15:17:50 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config(object):
-    # استدعاء القيم من متغيرات البيئة الصحيحة
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE")  # توكن البوت
-    APP_ID = int(os.environ.get("APP_ID", 7013440973))  # App ID الافتراضي
-    API_HASH = os.environ.get("API_HASH", "bcdae25b210b2cbe27c03117328648a2")  # API Hash الافتراضي
