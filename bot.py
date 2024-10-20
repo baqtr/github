a0e43a483b3dc1c3abe8ea1b52c167208bf70f7e
baqtr <102158404+baqtr@users.noreply.github.com>
2024-10-20 11:17:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,23 @@
-import pyrogram, pyromod
+import pyrogram , pyromod
 from config import Config 
 from pyromod import listen
 
 from pyrogram import Client, filters, enums
 from kvsqlite.sync import Client as dt
-
 p = dict(root='plugins')
-
-tok = "7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE"  # توكن البوت هنا
-id = 7013440973  # الايدي الخاص بك هنا
-
+tok = Config.TG_BOT_TOKEN 7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE ## توكنك 
+id = Config.APP_ID 7013440973 ## ايديك
 db = dt("data.sqlite", 'fuck')
 if not db.get("checker"):
-    db.set('checker', None)
+  db.set('checker', None)
 if not db.get("admin_list"):
-    db.set('admin_list', [id, 7013440973])
+  db.set('admin_list', [id, 7013440973])
 if not db.get('ban_list'):
-    db.set('ban_list', [])
+  db.set('ban_list', [])
 if not db.get('sessions'):
-    db.set('sessions', [])
+  db.set('sessions', [])
 if not db.get('force'):
-    db.set('force', ['ui_xb'])
-
-x = Client(name='loclhosst', api_id=id, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+  db.set('force', ['ui_xb'])
+x = Client(name='loclhosst', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 
 x.run()
