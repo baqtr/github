8a362836a65c5827a83e388122158a1a9d29a650
baqtr <102158404+baqtr@users.noreply.github.com>
2024-10-20 11:23:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
 
-
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE", "7013440973")
-
-    APP_ID = int(os.environ.get("APP_ID", 21669021))
-
-    API_HASH = os.environ.get("API_HASH", "bcdae25b210b2cbe27c03117328648a2")
+    # استدعاء القيم من متغيرات البيئة الصحيحة
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7137946160:AAEtwG3rYA9WwtWCUNsSVxERMPDJVrke-DE")  # توكن البوت
+    APP_ID = int(os.environ.get("APP_ID", 7013440973))  # App ID الافتراضي
+    API_HASH = os.environ.get("API_HASH", "bcdae25b210b2cbe27c03117328648a2")  # API Hash الافتراضي
